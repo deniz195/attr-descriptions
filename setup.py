fa2ab5b81c770548b485fea6772e451c727cbc42
Deniz Bozyigit <dbozyigit@battrion.com>
2019-06-19 10:47:31 +0200

    added stuff for v0.1.1

--- setup.py
+++ setup.py
@@ -12,13 +12,13 @@ with open('LICENSE') as f:
     license = f.read()
 
 setup(
-    name='json-sensor',
+    name='attr-descriptions',
     version='0.1.1',
-    description='A mini-framework that helps to read data from sensors in a distributed system or machine.',
+    description='A mini-module that helps to add descriptions to attrs attributes.',
     long_description=readme,
     author='Deniz Bozyigit',
     author_email='deniz195@gmail.com',
-    url='https://github.com/deniz195/json-sensor',
+    url='https://github.com/deniz195/attr-descriptions',
     license=license,
     packages=find_packages(exclude=('tests', 'docs'))
     install_requires = ['attr','cattrs','logging','functools','typing','math','pytest']
