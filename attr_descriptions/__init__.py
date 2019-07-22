a726f7d3c6cad0a17357651cf9ad99e91e5098ea
Deniz Bozyigit <dbozyigit@battrion.com>
2019-07-22 09:48:50 +0200

    extended with two functions describe and get

--- attr_descriptions/__init__.py
+++ attr_descriptions/__init__.py
@@ -13,11 +13,43 @@ module_logger = logging.getLogger(__name__)
 module_logger.setLevel(logging.DEBUG)
 
 
+def describe(attrib, description, significant_digits=None):
+    attrib.metadata['__description'] = description
+    attrib.metadata['__significant_digits'] = significant_digits
+    return attrib
+
+def get(cls, attr_name, default_description, default_significant_digits):
+    meta = attr.fields_dict(cls)[attr_name]
+    info = {}
+
+    try:
+        description = meta.metadata['__description']
+    except:
+        description = None
+    
+    try:
+        sig_digits = meta.metadata['__significant_digits']
+    except:
+        sig_digits = None
+
+    info['description'] = description if description is not None else default_description
+    info['significant_digits'] = sig_digits if sig_digits is not None else default_significant_digits
+
+    return info
+
+
 def add_attr_description(attrib, description=None):
     attrib.metadata['__description'] = description
     return attrib
 
-def get_attr_description(cls, attr_name):
+def get_attr_description(cls, attr_name, default=None):
     meta = attr.fields_dict(cls)[attr_name]
-    return meta.metadata['__description']
+
+    try:
+        return meta.metadata['__description']
+    except:
+        return default
+    
+
+
 
--- tests/test_get_attr_description.py
+++ tests/test_get_attr_description.py
@@ -1,3 +1,4 @@
+import pytest
 import logging
 
 import typing
@@ -8,13 +9,52 @@ import math
 from functools import reduce
 from attr_descriptions import *
 
-@attr.s
-class C(object):
-    x = add_attr_description(attr.ib(default=99), description='Some parameter')
-
 def test_add_attr_description():
+    @attr.s
+    class C(object):
+        x = add_attr_description(attr.ib(default=99), description='Some parameter')
+
     c = C()
     assert(attr.fields(C).x.metadata['__description']=='Some parameter')
+
+
+def test_get_attr_description():
+    @attr.s
+    class C(object):
+        x = add_attr_description(attr.ib(default=99), description='description for x')
+        y = add_attr_description(attr.ib(default=99), description='description for y')
+
+    x_des = get_attr_description(C, 'x')
+    assert(x_des =='description for x')
+    y_des = get_attr_description(C, 'y')
+    assert(y_des =='description for y')
+    with pytest.raises(KeyError):
+        get_attr_description(C, 'z')
+
+
+
+
+def test_describe():
+    @attr.s
+    class C(object):
+        x = describe(attr.ib(default=99), description='description for x', significant_digits=2)
+        y = describe(attr.ib(default=99), description='description for y', significant_digits=2)
+        z1 = attr.ib(default=99)
+        z2 = describe(attr.ib(default=99), description=None)
+
+    x_des = get_attr_description(C, 'x')
+    assert(x_des =='description for x')
+    y_des = get_attr_description(C, 'y')
+    assert(y_des =='description for y')
+
+    x_des = get(C, 'x', 'No description', 3)
+    assert(x_des == dict(description='description for x', significant_digits=2))
+
+    z_des = get(C, 'z1', 'No description', 3)
+    assert(z_des == dict(description='No description', significant_digits=3))
+
+    z_des = get(C, 'z2', 'No description', 3)
+    assert(z_des == dict(description='No description', significant_digits=3))
 
 
 
